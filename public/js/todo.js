6bc4cd98d2ec1d1b15952ba90ce315fa95e06925
nabarunsengupta <sengupta.nabarun@gmail.com>
2016-07-03 09:57:25 -0700

    Adding different routes to the features

--- public/js/todo.js
+++ public/js/todo.js
@@ -4,8 +4,9 @@ runuApp.config(['$routeProvider', function($routeProvider){
         $routeProvider.
             when('/styledetection', {templateUrl:'Partial/styledetection.html', controller: 'StyleDetectionController'}).
             when('/imagedetection', {templateUrl:'Partial/imagedetection.html', controller: 'ImageDetectionController'}).
-            when('/home', {templateUrl:'Partial/home.html', controller: 'HomeController'}).
-            otherwise({redirectTo:'/home', template:'Partial/home.html'});
+            when('/facedetection', {templateUrl:'Partial/facedetection.html', controller: 'FaceDetectionController'}).
+            when('/locationdetection', {templateUrl:'Partial/locationdetection.html', controller: 'LocationDetectionController'}).
+            otherwise({redirectTo:'/imagedetection', template:'Partial/imagedetection.html'});
     }]);
 
 runuApp.controller('StyleDetectionController', function($scope) {
@@ -21,9 +22,15 @@ runuApp.controller('ImageDetectionController', function($scope) {
 
 });
 
-runuApp.controller('HomeController', function($scope) {
+runuApp.controller('FaceDetectionController', function($scope) {
 
-    $scope.message = 'This is Home';
+    $scope.message = 'This is face detection';
+
+});
+
+runuApp.controller('LocationDetectionController', function($scope) {
+
+    $scope.message = 'This is new Location';
 
 });
 
